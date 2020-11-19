406cea746bbe75c93327786db4132837f154f2c9
459below <git@459below.org>
2020-11-19 22:45:56 +0100

    Changes to migrate to newer version of frameworks
    This is changing imports so that they reflect the
    respective new locations of the packages. Many of
    the dependencies have been moved around in TF2.

--- src/core/frontend.py
+++ src/core/frontend.py
@@ -1,17 +1,18 @@
-from keras.models import Model
-from keras.layers import Reshape, Activation, Conv2D, Input, MaxPooling2D, BatchNormalization, Flatten, Dense, Lambda
-from keras.layers.advanced_activations import LeakyReLU
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Reshape, Activation, Conv2D, Input, MaxPooling2D, BatchNormalization, Flatten, Dense, Lambda
+from tensorflow.keras.layers import LeakyReLU
 import tensorflow as tf
 import numpy as np
 import os
 import cv2
 from .utils import decode_netout, compute_overlap, compute_ap
-from keras.applications.mobilenet import MobileNet
-from keras.layers.merge import concatenate
-from keras.optimizers import SGD, Adam, RMSprop
+from tensorflow.keras.applications.mobilenet import MobileNet
+from tensorflow.keras.layers import concatenate
+from tensorflow.keras.optimizers import SGD, Adam, RMSprop
 from .preprocessing import BatchGenerator
-from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
-from keras.utils import multi_gpu_model, print_summary
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+from tensorflow.python.keras.utils.layer_utils import print_summary
+from tensorflow.python.keras.utils.multi_gpu_utils import multi_gpu_model
 from .backend import TinyYoloFeature, FullYoloFeature, MobileNetFeature, SqueezeNetFeature, Inception3Feature, VGG16Feature, ResNet50Feature
 from tensorflow.python.client import device_lib
 import rospy
