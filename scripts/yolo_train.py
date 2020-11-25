0cbf484d62da95beeea4eebd5d792c654b0513b1
459below <git@459below.org>
2020-11-25 19:30:20 +0100

    Updating call to 'parse_annotation_xml'
    The function 'parse_annotation' has been split into two seperate
    functions: parse_annotation_xml and parse_annotation_csv.
    
    We are using the parse_annotation_xml variant in this case.

--- scripts/yolo_train.py
+++ scripts/yolo_train.py
@@ -7,7 +7,7 @@ from __future__ import print_function
 import os
 import numpy as np
 import rospy
-from core import YOLO, parse_annotation
+from core import YOLO, parse_annotation_xml
 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]="0,1"
@@ -48,7 +48,7 @@ class YoloTrain(object):
         self.class_scale = rospy.get_param('~class_scale', default=1.0)
 
         # parse annotations of the training set
-        self.train_imgs, self.train_labels = parse_annotation(
+        self.train_imgs, self.train_labels = parse_annotation_xml(
             self.train_annot_folder, 
             self.train_image_folder, 
             self.labels)
@@ -57,7 +57,7 @@ class YoloTrain(object):
         if 'valid_annot_folder' in rospy.get_param_names():
             self.valid_annot_folder = rospy.get_param('~valid_annot_folder')
             self.valid_image_folder = rospy.get_param('~valid_image_folder')
-            self.valid_imgs, self.valid_labels = parse_annotation(
+            self.valid_imgs, self.valid_labels = parse_annotation_xml(
                 self.valid_annot_folder, 
                 self.valid_image_folder, 
                 self.labels)
